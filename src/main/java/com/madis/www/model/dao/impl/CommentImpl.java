810c77bc0072758982eb4e35f57202e59e2631ef
harrimkim <harrimkim@gmail.com>
2018-05-24 21:46:23 +0900

    CommentImpl repository 태그 추가

--- src/main/java/com/madis/www/model/dao/impl/CommentImpl.java
+++ src/main/java/com/madis/www/model/dao/impl/CommentImpl.java
@@ -3,10 +3,26 @@ package com.madis.www.model.dao.impl;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.madis.www.model.dao.CommentDao;
 import com.madis.www.model.dto.Comment;
 
-public class CommentImpl {
+@Repository
+public class CommentImpl implements CommentDao {
+	
+	private JdbcTemplate jdbcTemplate;
 
+	@Autowired
+	public void setDataSource(DataSource dataSource) {
+		jdbcTemplate = new JdbcTemplate(dataSource);
+	}
+	
+	@Override
 	public List<Comment> getCommentList(int index){
 		List<Comment> a = new ArrayList<Comment>();
 		return a;
